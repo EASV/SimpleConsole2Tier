63b0897d8bd359e3f7848bb703ee8c33794202af
lbilde <larsbilde@gmail.com>
2016-08-19 09:47:24 +0200

    added neat stuff

---
+++ DataLogicLayer/Entities/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLogicLayer.Entities
+{
+    public enum Gender
+    {
+        Male,
+        Female,
+        TheOthers
+    }
+    
+    public class Person
+    {
+        public int Id { get; internal set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Gender Gender { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} {Gender} - Born this year: {BirthDate.Year} - Email: {Email}";
+        }
+    }
+}
